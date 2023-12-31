86e3cf618b4c4820e9e9184152f2886d8cf1a47b
Victor medeiros <victorhugomedlima@gmail.com>
2023-12-31 16:52:49 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("998765", "modelo 5", "22222", 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("12345", "modelo 15", "5423", 256);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Tinder");
+iphone.ReceberLigacao();
